34581f19839c1fdcedb894792de034510f104b4b
arthur <arthur.rios007@gmail.com>
2024-10-05 10:58:55 -0300

    Initial Commit

---
+++ ReminderTests/ReminderTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderTests.swift
+//  ReminderTests
+//
+//  Created by Arthur Rios on 05/10/24.
+//
+
+import Testing
+@testable import Reminder
+
+struct ReminderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
