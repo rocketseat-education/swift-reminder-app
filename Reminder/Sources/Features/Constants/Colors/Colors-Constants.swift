6550811b95fc4c43fb0ef2cead9498726ec98cf1
arthur <arthur.rios007@gmail.com>
2024-10-08 21:32:32 -0300

    Construindo seu primeiro projeto - Como codar uma View

---
+++ Reminder/Sources/Features/Constants/Colors/Colors-Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Colors-Constants.swift
+//  Reminder
+//
+//  Created by Arthur Rios on 08/10/24.
+//
+
+import Foundation
--- Reminder/Sources/Features/Splash/SplashView.swift
+++ Reminder/Sources/Features/Splash/SplashView.swift
@@ -10,8 +10,25 @@ import UIKit
 
 class SplashView: UIView {
     
-    init() {
-        super.init(frame: .zero)
+    private let logoImageView: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "Logo")
+        image.contentMode = .scaleAspectFit
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    private let titleLogoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Remind"
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 32)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
         setupUI()
     }
     
@@ -20,11 +37,21 @@ class SplashView: UIView {
     }
     
     private func setupUI() {
-        self.backgroundColor = .blue
+        self.backgroundColor = UIColor(red: 192/255, green: 38/255, blue: 54/255, alpha: 1)
+        
+        self.addSubview(logoImageView)
+        self.addSubview(titleLogoLabel)
+        
         setupConstraints()
     }
     
     private func setupConstraints() {
-        
+        NSLayoutConstraint.activate([
+            titleLogoLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor, constant: 24),
+            titleLogoLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            
+            logoImageView.trailingAnchor.constraint(equalTo: titleLogoLabel.leadingAnchor, constant: -8),
+            logoImageView.bottomAnchor.constraint(equalTo: titleLogoLabel.bottomAnchor)
+        ])
     }
 }
