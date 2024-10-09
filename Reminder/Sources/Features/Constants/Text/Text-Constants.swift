f72846ecd3f0c3e0ff179ba27d4f4583e7daa231
arthur <arthur.rios007@gmail.com>
2024-10-08 21:56:02 -0300

    Construindo seu primeiro projeto - Criando seu System Design

---
+++ Reminder/Sources/Features/Constants/Text/Text-Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Text-Constants.swift
+//  Reminder
+//
+//  Created by Arthur Rios on 08/10/24.
+//
+
+import Foundation
+import UIKit
+
+public struct Typography {
+    static let heading = UIFont.systemFont(ofSize: 20, weight: .heavy)
+    static let subHeading = UIFont.systemFont(ofSize: 15, weight: .heavy)
+    static let input = UIFont.systemFont(ofSize: 16, weight: .regular)
+    static let label = UIFont.systemFont(ofSize: 14, weight: .heavy)
+    static let body = UIFont.systemFont(ofSize: 14, weight: .regular)
+    static let tag = UIFont.systemFont(ofSize: 12, weight: .semibold)
+}
--- Reminder/Sources/Features/Splash/SplashView.swift
+++ Reminder/Sources/Features/Splash/SplashView.swift
@@ -18,15 +18,6 @@ class SplashView: UIView {
         return image
     }()
     
-    private let titleLogoLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Remind"
-        label.textColor = .white
-        label.font = UIFont.boldSystemFont(ofSize: 32)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupUI()
@@ -37,21 +28,15 @@ class SplashView: UIView {
     }
     
     private func setupUI() {
-        self.backgroundColor = UIColor(red: 192/255, green: 38/255, blue: 54/255, alpha: 1)
-        
         self.addSubview(logoImageView)
-        self.addSubview(titleLogoLabel)
         
         setupConstraints()
     }
     
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            titleLogoLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor, constant: 24),
-            titleLogoLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            
-            logoImageView.trailingAnchor.constraint(equalTo: titleLogoLabel.leadingAnchor, constant: -8),
-            logoImageView.bottomAnchor.constraint(equalTo: titleLogoLabel.bottomAnchor)
+            logoImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            logoImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -Metrics.medium),
         ])
     }
 }
