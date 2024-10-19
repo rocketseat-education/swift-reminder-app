dcbb9299e286ab8f2348be49bc310105caf22397
arthur <arthur.rios007@gmail.com>
2024-10-19 09:11:19 -0300

    Regras de negócio e arquitetura - Criando UserDefaults

---
+++ Reminder/Sources/Features/Model/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Reminder
+//
+//  Created by Arthur Rios on 19/10/24.
+//
+
+import Foundation
+
+struct User: Codable {
+    let email: String
+    let isUserSaved: Bool
+}
